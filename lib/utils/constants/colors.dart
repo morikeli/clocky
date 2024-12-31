7cc5e9ea52b0908bf2dface7afdc45b0a2108572
magnus.coder <kelimbth@gmail.com>
2024-12-31 23:56:30 +0300

    added colors

---
+++ lib/utils/constants/colors.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+
+const kPrimaryColor = Color(0xFF0D47A1);
+const kAppBarBgColor = Color(0xFFFFFFFF);
+const kAppBarBgDarkColor = Color(0xFF000000);
+const kScaffoldBgColor = Color(0xFFFFFFFF);
+const kScaffoldDarkBgColor = Color(0xFF000000);
+const kSecondaryColor = Color(0xFFE4E9F2);
+const kSecondaryDarkColor = Color(0xFF404040);
+const kShadowColor = Color(0xFF364564);
+const kAccentColor = Color(0xFFB3BFD7);
+const kAccentDarkColor = Color(0xFF4E4E4E);
+const kBackgroundDarkColor = Color(0xFF3A3A3A);
+const kSurfaceColor = Color.fromARGB(255, 237, 237, 241);
+const kSurfaceDarkColor = Color(0xFF222225);
+
+const kAccentIconColor = Color(0xFFECEFF5);
+const kAccentIconDarkColor = Color(0xFF303030);
+const kPrimaryIconColor = Color(0xFFECEFF5);
+const kPrimaryIconDarkColor = Color(0xFF232323);
+
+const kTextColor = Color(0xFFA1B0CA);
+const kTextDarkColor = Color(0xFF7C7C7C);
+const kTitleColor = Color(0xFF101112);
+const kTitleTextDarkColor = Color(0xFF000000);
+
+const kButtonBlueAccentColor = Color(0xFF1976D2);
