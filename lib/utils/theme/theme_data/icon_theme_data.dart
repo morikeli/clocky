a92df2ec0308ace4eed82c8a1ea5eacd89ed6b22
magnus.coder <kelimbth@gmail.com>
2024-12-31 23:58:51 +0300

    icon theme data

---
+++ lib/utils/theme/theme_data/icon_theme_data.dart
@@ -0,0 +1,18 @@
+import 'package:clock_app/utils/constants/colors.dart';
+import 'package:flutter/material.dart';
+
+
+class ClockAppIconThemeData {
+  ClockAppIconThemeData._();
+
+  // Light mode
+  static IconThemeData iconThemeData = const IconThemeData(
+    color: kPrimaryIconDarkColor,
+  );
+
+  // 2. Dark mode
+  static IconThemeData iconDarkThemeData = const IconThemeData(
+    color: kPrimaryIconDarkColor,
+  );
+  
+}
