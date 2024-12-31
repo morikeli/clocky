438e0ebddcdef506efa2fc538100375ea7042aba
magnus.coder <kelimbth@gmail.com>
2025-01-01 00:06:17 +0300

    to do task

---
+++ lib/utils/timer.dart
@@ -0,0 +1,3 @@
+// TODO: Change the background color of the app depending on daylight
+// if its early day hours, white opacity is maximum then decrease as the day progresses
+// if its night, black opacity is maximum and decrease as the night progresses.
